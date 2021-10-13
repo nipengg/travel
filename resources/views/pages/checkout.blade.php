d5ba533435304c0e5ae08008b3429a1fa469a284
nipengg <nevillecorneliustj@gmail.com>
2021-10-13 22:43:46 +0700

    Checkout Integration

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -29,9 +29,20 @@
         <div class="row">
           <div class="col-lg-8 pl-lg-0">
             <div class="card card-details mb-3">
+
+              @if ($errors->any())
+                  <div class="alert alert-danger">
+                      <ul>
+                        @foreach ($errors->all as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                      </ul>
+                  </div>
+              @endif
+
               <h1>Who is Going?</h1>
               <p>
-                Trip to Ubud, Bali, Indonesia
+                Trip to {{ $item->travel_package->title }}, {{ $item->travel_package->location }}
               </p>
               <div class="attendee">
                 <table class="table table-responsive-sm text-center">
@@ -46,79 +57,77 @@
                     </tr>
                   </thead>
                   <tbody>
+                    @forelse ($item->details as $detail)
                     <tr>
                       <td>
                         <img
-                          src="{{ url('frontend/images/avatar-4.png') }}"
-                          alt=""
+                          src="https://ui-avatars.com/api/?name={{ $detail->username }}"
+                          class="rounded-circle"
                           height="60"
                         />
                       </td>
-                      <td class="align-middle">Angga Risky</td>
-                      <td class="align-middle">CN</td>
-                      <td class="align-middle">N/A</td>
-                      <td class="align-middle">Active</td>
+                      <td class="align-middle">{{ $detail->username }}</td>
+                      <td class="align-middle">{{ $detail->nationality }}</td>
+                      <td class="align-middle">{{ $detail->is_visa ? '30 days' : 'N/A' }}</td>
+                      <td class="align-middle">{{ \Carbon\Carbon::createFromDate($detail->doe_passport) > \Carbon\Carbon::now() ? 'Active' : 'Inactive' }}</td>
                       <td class="align-middle">
-                        <a href="#">
-                          <img src="{{ url('frontend/images/ic_remove.png') }}" alt="" />
-                        </a>
+                        <form action="{{ route('checkout-remove', $detail->id) }}" method="POST">
+                          @csrf
+                          <button style="background: transparent; border: none" type="submit"><img src="{{ url('frontend/images/ic_remove.png') }}" alt="" /></button>
+                        </form>
                       </td>
                     </tr>
+                    @empty
                     <tr>
-                      <td>
-                        <img
-                          src="{{ url('frontend/images/avatar-5.png') }}"
-                          alt=""
-                          height="60"
-                        />
-                      </td>
-                      <td class="align-middle">Galih Pratama</td>
-                      <td class="align-middle">SG</td>
-                      <td class="align-middle">30 Days</td>
-                      <td class="align-middle">Active</td>
-                      <td class="align-middle">
-                        <a href="#">
-                          <img src="{{ url('frontend/images/ic_remove.png') }}" alt="" />
-                        </a>
+                      <td colspan="6" class="text-center">
+                        No Visitor
                       </td>
                     </tr>
+                    @endforelse
                   </tbody>
                 </table>
               </div>
               <div class="member mt-3">
                 <h2>Add Member</h2>
-                <form class="form-inline">
-                  <label class="sr-only" for="inputUsername">Name</label>
+                <form class="form-inline" method="POST" action="{{ route('checkout-create', $item->id) }}">
+                  @csrf
+                  <label class="sr-only" for="username">Name</label>
                   <input
                     type="text"
+                    name="username"
                     class="form-control mb-2 mr-sm-2"
-                    id="inputUsername"
+                    id="username"
                     placeholder="Username"
+                    required
                   />
 
-                  <label
-                    class="sr-only"
-                    class="mr-2"
-                    for="inlineFormCustomSelectPref"
-                    >Preference</label
-                  >
-                  <select
-                    class="custom-select mb-2 mr-sm-2"
-                    id="inlineFormCustomSelectPref"
-                  >
+                  <label class="sr-only" for="nationality">Nationality</label>
+                  <input
+                    type="text"
+                    name="nationality"
+                    class="form-control mb-2 mr-sm-2"
+                    style="width: 50px"
+                    id="nationality"
+                    placeholder="Nationality"
+                    required
+                  />
+
+                  <label class="sr-only mr-2" for="is_visa">VISA</label>
+                  <select class="custom-select mb-2 mr-sm-2" id="is_visa" name="is_visa" required>
                     <option selected value="">VISA</option>
-                    <option value="2">30 Days</option>
-                    <option value="3">N/A</option>
+                    <option value="1">30 Days</option>
+                    <option value="0">N/A</option>
                   </select>
 
-                  <label class="sr-only" for="doePassport"
+                  <label class="sr-only" for="doe_passport"
                     >DOE Passport</label
                   >
                   <div class="input-group mb-2 mr-sm-2">
                     <input
                       type="text"
                       class="form-control datepicker"
-                      id="doePassport"
+                      name="doe_passport"
+                      id="doe_passport"
                       placeholder="DOE Passport"
                     />
                   </div>
@@ -129,8 +138,7 @@
                 </form>
                 <h3 class="mt-2 mb-0">Note</h3>
                 <p class="disclaimer mb-0">
-                  You are only able to invite member that has registered in
-                  Nomads.
+                  You are only able to invite member that has registered.
                 </p>
               </div>
             </div>
@@ -141,25 +149,25 @@
               <table class="trip-informations">
                 <tr>
                   <th width="50%">Members</th>
-                  <td width="50%" class="text-right">2 person</td>
+                  <td width="50%" class="text-right">{{ $item->details->count() }} person</td>
                 </tr>
                 <tr>
                   <th width="50%">Additional Visa</th>
-                  <td width="50%" class="text-right">$ 190,00</td>
+                  <td width="50%" class="text-right">$ {{ $item->additional_visa }},00</td>
                 </tr>
                 <tr>
                   <th width="50%">Trip Price</th>
-                  <td width="50%" class="text-right">$ 80,00 / person</td>
+                  <td width="50%" class="text-right">$ {{ $item->travel_package->price }},00 / person</td>
                 </tr>
                 <tr>
                   <th width="50%">Sub Total</th>
-                  <td width="50%" class="text-right">$ 280,00</td>
+                  <td width="50%" class="text-right">$ {{ $item->transaction_total }},00</td>
                 </tr>
                 <tr>
                   <th width="50%">Total (+Unique)</th>
                   <td width="50%" class="text-right text-total">
-                    <span class="text-blue">$ 279,</span
-                    ><span class="text-orange">33</span>
+                    <span class="text-blue">$ {{ $item->transaction_total }},</span
+                    ><span class="text-orange">{{ mt_rand(0,99) }}</span>
                   </td>
                 </tr>
               </table>
@@ -178,9 +186,9 @@
                     class="bank-image"
                   />
                   <div class="description">
-                    <h3>PT Nomads ID</h3>
+                    <h3>PT Moli</h3>
                     <p>
-                      0881 8829 8800
+                      08** *** ****
                       <br />
                       Bank Central Asia
                     </p>
@@ -194,9 +202,9 @@
                     class="bank-image"
                   />
                   <div class="description">
-                    <h3>PT Nomads ID</h3>
+                    <h3>PT Nipeng</h3>
                     <p>
-                      0899 8501 7888
+                      08** *** ****
                       <br />
                       Bank HSBC
                     </p>
@@ -207,13 +215,13 @@
             </div>
             <div class="join-container">
               <a
-                href="{{ url('/checkout/success') }}"
+                href="{{ route('checkout-success', $item->id) }}"
                 class="btn btn-block btn-join-now mt-3 py-2"
                 >I Have Made Payment</a
               >
             </div>
             <div class="text-center mt-3">
-              <a href="{{ url('/detail') }}" class="text-muted">Cancel Booking</a>
+              <a href="{{ route('detail', $item->travel_package->slug) }}" class="text-muted">Cancel Booking</a>
             </div>
           </div>
         </div>
@@ -231,6 +239,7 @@
     <script>
         $(document).ready(function() {
           $('.datepicker').datepicker({
+            format: 'yyyy-mm-dd',
             uiLibrary: 'bootstrap4',
             icons: {
               rightIcon: '<img src="{{ url('frontend/images/ic_doe.png') }}" alt="" />'
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/detail/{slug}', 'DetailController@index')->name('detail');
 Route::post('/checkout/{id}','CheckoutController@process')->name('checkout_process')->middleware(['auth', 'verified']);
 Route::get('/checkout/{id}','CheckoutController@index')->name('checkout')->middleware(['auth', 'verified']);
 Route::post('/checkout/create/{detail_id}','CheckoutController@create')->name('checkout-create')->middleware(['auth', 'verified']);
-Route::post('/checkout/remove{detail_id}','CheckoutController@remove')->name('checkout-remove')->middleware(['auth', 'verified']);
+Route::post('/checkout/remove/{detail_id}','CheckoutController@remove')->name('checkout-remove')->middleware(['auth', 'verified']);
 Route::get('/checkout/confirm/{id}','CheckoutController@success')->name('checkout-success')->middleware(['auth', 'verified']);
 
 Route::prefix('admin')
